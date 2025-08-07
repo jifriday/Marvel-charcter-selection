84facd63cd79ba0baf0e076f32bfb988cb5c9e5f
Joseph inalegewu Friday <jifriday@abu.edu.ng>
2025-08-07 20:26:31 +0000

    Bro

---
+++ main.py
@@ -0,0 +1,19 @@
+print("\033[31m""Marvel Movie Character Creator""\033[0m")
+print()
+print("--")
+print()
+hanging_around = input("Do you like hanging around ?:")
+if hanging_around == "yes":
+    print("Then you're more likely to be","\033[32m""Spider-Man""\033[0m")
+else:
+  print("Then you're not","\033[32m""Spider-man""\033[0m")
+gravelling = input("Do you have a gravelly voice?:")
+if gravelling == "yes":
+    print("Then you're more likely to be","\033[34m""Korg""\033[0m")
+else :
+  print("Then you're not","\033[34m""Korg""\033[0m")
+marvelous = input("Do you often feel marvelous ?:")
+if marvelous == "yes":
+    print("Aha! You're","\033[35m""Captain Marvel""\033[0m""! Hi!")
+else :
+  print("Then you're not","\033[35m""Captain Marvel""\033[0m")
